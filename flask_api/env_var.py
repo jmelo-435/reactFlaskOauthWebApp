c454f038f7b3733ba4c470202d961900d0839d28
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-08-31 18:51:29 -0300

    adds url for resource api

--- flask_api/env_var.py
+++ flask_api/env_var.py
@@ -32,7 +32,7 @@ def get_app_secret_key():
     
 def get_create_resource_user_address():
     if os.environ['FLASK_ENV']=='development':
-        return '/api_res/users'
+        return 'http://localhost/api_res/users'
     if os.environ['FLASK_ENV']=='production':
         return os.environ['CREATE_RESOURCE_USER_ADDRESS']
 
