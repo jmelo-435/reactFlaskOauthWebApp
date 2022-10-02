45051c702d7705a626790d9feefb232b66688803
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-10-02 19:02:06 -0300

    proffits shows on proffits list

--- react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
+++ react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
@@ -1,7 +1,9 @@
 import "./StockProffitDisplay.css"
-const StockProffitDisplay=({profit})=>{
+const StockProffitDisplay=({year,profit})=>{
     return(
         <div className="stockProffitDisplay">
+        <h6>{year}</h6>
+        <h6>:</h6>
         <h6>{profit}</h6>
         </div>
         
