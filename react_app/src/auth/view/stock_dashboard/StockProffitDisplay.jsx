714d5ff5c992e582393e02d4d9fd6d0b1dfee5de
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-10-03 09:51:38 -0300

    customizes proffit card appearance

--- react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
+++ react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
@@ -3,7 +3,7 @@ const StockProffitDisplay=({year,profit})=>{
     return(
         <div className="stockProffitDisplay">
         <h1>{year}</h1>
-        <div className = {profit>0 ? "posProffitDisplay" : "negProffitDisplay"}>
+        <div className = {parseInt(profit)>0 ? "posProffitDisplay" : "negProffitDisplay"}>
             <span><h6>{profit}</h6></span>
         </div>
         </div>
