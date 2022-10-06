4746e92c609e454994c3d924b06434612eddd6d4
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-10-06 10:17:48 -0300

    implements sotck data display on maindashboard

--- react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
+++ react_app/src/auth/view/stock_dashboard/StockProffitDisplay.jsx
@@ -4,9 +4,9 @@ const StockProffitDisplay=({year,profit})=>{
         <div className="stockProffitDisplay">
         <h1>{year}</h1>
         <div className = {parseInt(profit)>0 ? "posProffitDisplay" : "negProffitDisplay"}>
-        <div className = {Math.abs(parseInt(profit))>=1000 ? "bigProffitDisplay" : "smallProffitDisplay"}>
+        <div className = {Math.sign(parseInt(profit))*parseInt(profit)>=1000 ? "bigProffitDisplay" : "smallProffitDisplay"}>
             {
-            Math.abs(parseInt(profit))>=1000?
+            Math.sign(parseInt(profit))*parseInt(profit)>=1000?
             <span><h6>{Math.round(parseInt(profit)/1000)} bi</h6></span>
             :
             <span><h6>{profit}mi</h6></span>
