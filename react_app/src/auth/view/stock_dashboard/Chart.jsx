525a1c0599d440509e135b364f1e6525ec0521cd
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-10-08 11:31:05 -0300

    implements rechart library

--- react_app/src/auth/view/stock_dashboard/Chart.jsx
+++ react_app/src/auth/view/stock_dashboard/Chart.jsx
@@ -2,14 +2,14 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 const Chart =({data})=>{
     return(
         <div>
-            const renderLineChart = (
+            
   <LineChart width={600} height={300} data={data}>
     <Line type="monotone" dataKey="close" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" />
     <XAxis dataKey="date" />
     <YAxis />
   </LineChart>
-);
+
         </div>
     )
 
