fd1076b63430f38a2e5bef51b1249ffa8eae01bb
jmelo-435 <jefersonmedeiros000@gmail.com>
2022-10-09 10:07:25 -0300

    implements rechart library

--- react_app/src/auth/view/stock_dashboard/Chart.jsx
+++ react_app/src/auth/view/stock_dashboard/Chart.jsx
@@ -21,6 +21,18 @@ const Chart =({chartData})=>{
     <XAxis dataKey="date" />
     <YAxis />
   </AreaChart>
+  <AreaChart width={600} height={300} data={chartData}
+  margin={{
+    top: 10,
+    right: 30,
+    left: 0,
+    bottom: 0,
+  }}>
+    <Area type="monotone" dataKey="lucro" fill="black" fillOpacity={0.5}/>
+    <Tooltip/>
+    <XAxis dataKey="date" />
+    <YAxis />
+  </AreaChart>
 
         </div>
     )
